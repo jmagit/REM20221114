85a242ac34acba58155976abf8db56afb1abfaf4
Javier <jmartin@grupoloyal.com>
2022-11-15 11:36:52 +0100

    CRUD

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
+
+import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -11,9 +16,31 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo");
+		System.out.println("Arranque completado");
+		
+//		var actor = new Actor(217, "pepito", "PEREZ");
+//		dao.deleteById(215);
+//		var item = dao.findById(217);
+//		if(item.isPresent()) {
+//			var actor = item.get();
+//			actor.setFirstName(actor.getFirstName().toUpperCase());
+//			dao.save(actor);			
+//		} else {
+//			System.out.println("No encontrado");
+//		}
+//		dao.findAll().forEach(System.out::println);
+//		dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("p").forEach(System.out::println);
+//		dao.findTop5ByFirstNameStartingWith("p", Sort.by("firstName").descending()).forEach(System.out::println);
+		dao.findByActorIdGreaterThanEqual(200).forEach(System.out::println);
+		dao.findNovedadesJPQL(200).forEach(System.out::println);
+		dao.findNovedadesSQL(200).forEach(System.out::println);
+		dao.findAll((root, query, builder) -> builder.greaterThanOrEqualTo(root.get("actorId"), 200)).forEach(System.out::println);
+		dao.findAll((root, query, builder) -> builder.lessThanOrEqualTo(root.get("actorId"), 10)).forEach(System.out::println);
 	}
 
 }
---
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -0,0 +1,23 @@
+package com.example.domains.contracts.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.domains.entities.Actor;
+
+public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
+	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
+	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orden);
+	
+	List<Actor> findByActorIdGreaterThanEqual(int idInicial);
+	@Query("SELECT a FROM Actor a WHERE a.actorId >= ?1")
+	List<Actor> findNovedadesJPQL(int idInicial);
+	@Query(value =  "SELECT * FROM actor a WHERE a.actor_id >= ?1", nativeQuery = true)
+	List<Actor> findNovedadesSQL(int idInicial);
+	
+	
+}
