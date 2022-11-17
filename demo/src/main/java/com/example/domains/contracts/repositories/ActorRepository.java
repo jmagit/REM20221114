30a9ea5bc37d67e01b797060e14abd775f847209
Javier <jmartin@grupoloyal.com>
2022-11-17 11:35:45 +0100

    Data Rest

--- demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -8,11 +8,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.domains.entities.Actor;
 import com.example.domains.entities.dtos.ActorDTO;
 import com.example.domains.entities.dtos.ActorShort;
 
+@RepositoryRestResource(path="actores", itemResourceRel="actor", collectionResourceRel="actores")
 public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
 	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
 	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orden);
@@ -28,8 +31,14 @@ public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpeci
 	@Query("SELECT a FROM Actor a")
 	List<ActorDTO> dameTodosDto();
 	
+	@RestResource(exported = false)
 	<T> Iterable<T> findAllBy(Class<T> proyeccion);
+	@RestResource(exported = false)
 	<T> Iterable<T> findAllBy(Sort orden, Class<T> proyeccion);
+	@RestResource(exported = false)
 	<T> Page<T> findAllBy(Pageable page, Class<T> proyeccion);
 
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Integer id);
 }
--- demo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
+++ demo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
@@ -1,7 +1,11 @@
 package com.example.domains.entities.dtos;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
 
+import com.example.domains.entities.Actor;
+
+@Projection(name = "ActorShort", types = Actor.class)
 public interface ActorShort {
 	int getActorId();
 	@Value("#{target.lastName + ', ' + target.firstName}")
